1d8c925e65dbf8e72b2ed95859074de8ed8185bc
prmths128 <plloret@gmx.com>
2014-05-01 18:19:13 +0200

    Added random forest.

--- bakfu/classify/ml/forest.py
+++ bakfu/classify/ml/forest.py
@@ -13,9 +13,23 @@ from sklearn.ensemble import RandomForestClassifier
 
 @register('ml.forest')
 class ForestMl(BaseMl):
-    '''ForestMl
+    '''Random forest
+Parameters (not implemented yet) : 
+classifier : if not specified, create a new classifier.    
+data : if not specified, get most recent data from the chain.
+
+Usage : 
+#create clusters
+baf.process('cluster.ward')
+#load new data
+baf.load('data.simple',(...))
+#train classifier and use 
+baf.process('ml.forest')
+
+TODO: Allow reuse of classifiers.
     '''
-    init_kwargs = ('n_estimators', )
+    init_kwargs = ('n_estimators', 'classifier', 'data')
+    run_kwargs = ('action')
     #max_depth
     init_method = RandomForestClassifier.__init__
     run_method = RandomForestClassifier.fit_transform
@@ -25,40 +39,27 @@ class ForestMl(BaseMl):
 
         n_estimators = kwargs.get('n_estimators', 10)
 
-        self.classifier = RandomForestClassifier(n_estimators=10)
+        self.classifier = RandomForestClassifier(n_estimators=n_estimators)
         self._data['classifier'] = self.classifier
 
-
-    def clusterize(self, data_source, *args, **kwargs):
-        '''
-
-        '''
-
-        #>>> X = [[0, 0], [1, 1]]
-        #>>> Y = [0, 1]
-        #>>> clf = RandomForestClassifier(n_estimators=10)
-        #>>> clf = clf.fit(X, Y)
-
-        #self.result = {'clusters':result}
-        self.data['result'] = 42
-
-        return result
-
     def run(self, caller, *args, **kwargs):
-        super(ForestMl, self).run(*args, **kwargs)
+        super(ForestMl, self).run(caller,*args, **kwargs)
 
-        data_source = caller.data['main_data']
-        #vectorizer_result = caller.data['vectorizer_result']
-        vectorizer_result = self.get('vectorizer_result')
+        vectorizer_result = caller.get_chain('vectorizer_result')
+        clusters = caller.get("clusterizer_result")
+        vectorizer = caller.get_chain('vectorizer')
+        self.classifier.fit(vectorizer_result.toarray(), clusters)
 
-        X = StandardScaler(with_mean=False).fit_transform(vectorizer_result)
-        db = self.clusterizer.fit(X)
+        #New data
+        data_source = caller.get_chain("data_source")
+        new_vectorizer_result = vectorizer.transform(data_source.get_data())
 
-        caller.data['clusterizer_result'] = db
+        result = self.classifier.predict(new_vectorizer_result.toarray())
 
         return self.update(
-            result=db,
-            clusterizer_result=db)
+            result=result,
+            classifier_result=result
+            )
 
 
 
