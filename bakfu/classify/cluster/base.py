ba5063b6d807cf97aea456c0825851685a3f4fd4
prmths128 <plloret@gmx.com>
2014-05-24 16:35:45 +0200

    some refactoring

--- bakfu/classify/cluster/base.py
+++ bakfu/classify/cluster/base.py
@@ -2,19 +2,28 @@
 
 from abc import abstractmethod
 from ...core.classes import Processor
+from ..base import SklearnClassifier
 
-
-class BaseClusterizer(Processor):
+class BaseClusterizer(SklearnClassifier):
     '''Base class for vectorizers. 
     '''
     def __init__(self, *args, **kwargs):
         super(BaseClusterizer, self).__init__( *args, **kwargs)
+
+        #deprecated : use classifier
         self.clusterizer = None
 
-    def __getattr__(self, attr):
-        '''Propagate attribute search to the clusterizer.'''
-        try:
-            return getattr(self, attr)
-        except:
-            return getattr(self.clusterizer, attr)
+    def run_classifier(self, caller, *args, **kwargs):
+        data_source = caller.data['main_data']
+        vectorizer_result = caller.data['vectorizer_result']
+
+        X = vectorizer_result
+        predictions = self.classifier.fit_predict(X.toarray())
+
+        caller.data['clusterizer_result'] = predictions
+        caller.data['clusterizer'] = self.classifier
+
+        return self.update(
+            result=predictions,
+            classifier_result=predictions,)
 
--- bakfu/classify/cluster/ward_sklearn.py
+++ bakfu/classify/cluster/ward_sklearn.py
@@ -14,33 +14,34 @@ from .base import BaseClusterizer
 
 @register('cluster.ward')
 class WardClusterizer(BaseClusterizer):
-    
+
     init_kwargs = ('n_clusters',)
+    classifier_class = Ward
 
-    def __init__(self, *args, **kwargs):
-        super(WardClusterizer, self).__init__(*args, **kwargs)
+    #def __init__(self, *args, **kwargs):
+        #super(WardClusterizer, self).__init__(*args, **kwargs)
 
-        self.clusterizer = Ward(**kwargs)
+        #self.clusterizer = Ward(**kwargs)
 
-    def clusterize(self, data_source, *args, **kwargs):
-        '''
+    #def clusterize(self, data_source, *args, **kwargs):
+        #'''
 
-        '''
+        #'''
 
-        self.results = {'clusters':result}
+        #self.results = {'clusters':result}
 
-        return result
+        #return result
 
-    def run(self, caller, *args, **kwargs):
-        data_source = caller.data['main_data']
-        vectorizer_result = caller.data['vectorizer_result']
+    #def run(self, caller, *args, **kwargs):
+        #data_source = caller.data['main_data']
+        #vectorizer_result = caller.data['vectorizer_result']
 
-        X = vectorizer_result
-        predictions = self.clusterizer.fit_predict(X.toarray())
+        #X = vectorizer_result
+        #predictions = self.clusterizer.fit_predict(X.toarray())
 
-        caller.data['clusterizer_result'] = predictions
-        caller.data['clusterizer'] = self.clusterizer
+        #caller.data['clusterizer_result'] = predictions
+        #caller.data['clusterizer'] = self.clusterizer
 
-        return self.update(
-            result=predictions,
-            clusterizer_result=predictions)
+        #return self.update(
+            #result=predictions,
+            #clusterizer_result=predictions)
